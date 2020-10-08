478474b4192038670765a6722420d3996dd3fcda
SBuchanan1 <syd.buch@gmail.com>
2020-10-08 00:07:06 -0400

    added employee.js

--- employee.js
+++ employee.js
@@ -1,1 +1,51 @@
-require
+const mysql = require("mysql"),
+const inquirer = require("inquirer"),
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "developer22",
+    database: "employee_trackerdb"
+});
+
+let query = "SELECT employee, role, department";
+console.log(query);
+connection.connect(function (err) {
+    if (err) throw err;
+    employeePrompt();
+});
+
+function employeePrompt() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "rawlist",
+            message: "What would you like to do?",
+            choice: ["Add Employee", "Delete Employee"],
+        })
+        .then(function (answer)) {
+        switch (answer.action) {
+            case: "Add Employee":
+                addEmployee();
+                break;
+
+            case "Delete Employee":
+                deleteEmployee();
+                break;
+        }
+    }
+
+}
+
+// Function
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                name: "employee_first_name",
+                type: "input",
+                message: "What is the employee's first name?"
+            }
+        ])
+}
--- employeeseeds.sql
+++ employeeseeds.sql
@@ -4,45 +4,56 @@ USE employee_trackerdb;
 
 CREATE TABLE departments
 (
-    id INT
-    unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    id INT NOT NULL
+    AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR
-    (30) NOT NULL);
+    (30) NOT NULL
+);
+
 
     CREATE TABLE role
     (
-        id INT
-        unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-        title VARCHAR
-        (30) NOT NULL, 
-        salary DECIMAL unsigned NOT NULL, 
-        department_id INT unsigned NOT NULL,
-        INDEX sep_index
+        id INT NOT NULL
+        AUTO_INCREMENT PRIMARY KEY, 
+     title VARCHAR
+        (30) NOT NULL,
+     salary DECIMAL NOT NULL, 
+    department_id INT NOT NULL, 
+    INDEX dep_index
         (department_id),
-        CONSTRAINT fk_department FOREIGN KEY
+    CONSTRAINT fk_department FOREIGN KEY
         (department_id) REFERENCES departments
-        (id)ON
-        DELETE CASCADE);
+        (id)
+        ON
+        DELETE CASCADE
+    ):
+
+
+
+
+
+
 
         CREATE TABLE employee
         (
-            id INT
-            unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY, first_name VARCHAR
+            id INT NOT NULL
+            AUTO_INCREMENT PRIMARY KEY, 
+first_name VARCHAR
             (30) NOT NULL, 
-            last_name VARCHAR
+last_name VARCHAR
             (30) NOT NULL, 
-            role_id INT unsigned NOT NULL, 
-            INDEX role_inde
+role_id INT NOT NULL, 
+INDEX role_index
             (role_id),
-            CONSTRAINT fk_role FOREIGN KEY,
+CONSTRAINT fk_role FOREIGN KEY
             (role_id) REFERENCES role
-            (id) ON
-            DELETE CASCADE, manager_id INT unsigned, 
-            INDEX man_index
+            (id)ON
+            DELETE CASCADE, 
+manager_id INT , 
+INDEX man_index
             (manager_id),
-            CONSTRAINT fk_manager FOREIGN KEY
-            (manager_id) 
-            REFERENCES employee
+CONSTRAINT fk_manager FOREIGN KEY,
+            (manager_id) REFERENCES employee
             (id) ON
             DELETE
             SET NULL
